1331a5e6b3fe471a972ece025926cbb612ed7e05
Volodymyr <volodiax@gmail.com>
2018-12-11 13:36:04 +0200

    First commit JS

---
+++ start/script.js
@@ -0,0 +1,17 @@
+// study JavaScript kurs form Udemi
+var number=5;
+console.log(number);
+let string="yellow submarin";
+console.log(string);
+let arr=["Red","Blue","Pink","Black"];
+// alert("I am here!");
+
+// let answer=confirm("  It's Ok?");
+
+let anser=prompt("Есь ли тебе 18 лет", "Конечно!!!")
+
+
+console.log(anser+"   it's anser");
+console.log(typeof(anser));
+
+
